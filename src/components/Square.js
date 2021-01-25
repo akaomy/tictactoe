0ac310baaa98ab445366c862f8ae4452063f7a4f
akozlova <annakozlova92@gmail.com>
2021-01-24 19:45:07 -0800

    add initial ui board

---
+++ src/components/Square.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import '../App.css';
+
+export default function Square() {
+    return (
+        <li className="square">
+            <span className="letter">X</span>
+            {/* <span className="letter">O</span> */}
+        </li>
+    )
+}
